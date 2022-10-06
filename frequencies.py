598a4cb8b525136e6232633e2954d5146bdb21db
Alkhodir, Jana (k20061055) <jana.alkhodir@kcl.ac.uk>
2022-10-06 18:02:10 +0100

    Fix mistake

--- frequencies.py
+++ frequencies.py
@@ -3,10 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
+    j = 0
+    ctr = 0
     for i in range(len(items)):
-        frequencies[items[i]] = ctr
-        ctr = 0
-        for j in range(i):
-            if items[i] == str(items[j]):
-                ctr += 1
+        if items[j] == str[items[i]]:
+            ctr += 1
+    frequencies[items[i]] = ctr
+    ctr = 0
+    j += 1
     return frequencies
